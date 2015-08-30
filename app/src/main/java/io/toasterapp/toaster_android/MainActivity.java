c894032d6dd4faaf1d41756184c99908088868a9
Jenny Kim <yk494@cornell.edu>
2015-08-30 17:33:23 -0400

    autofocus on new post + opens keyboard

--- app/src/main/java/io/toasterapp/toaster_android/MainActivity.java
+++ app/src/main/java/io/toasterapp/toaster_android/MainActivity.java
@@ -1,12 +1,14 @@
 package io.toasterapp.toaster_android;
 
 import android.app.Activity;
+import android.content.Context;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
+import android.view.inputmethod.InputMethodManager;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
@@ -48,12 +50,28 @@ public class MainActivity extends Activity {
         settings.setUseWideViewPort(true);
         mWebView.setWebViewClient(new MyCustomWebViewClient());
         mWebView.setScrollBarStyle(View.SCROLLBARS_INSIDE_OVERLAY);
-        if (savedInstanceState == null) {
+
+//        mWebView.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+//
+//            public void onFocusChange(View arg0, boolean hasFocus) {
+//
+//                if (!hasFocus) {
+//                    Log.d("hasFocus", "false");
+//                    mWebView.requestFocus(View.FOCUS_DOWN);
+//                    mWebView.setFocusable(true);
+//                }
+//
+//                InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+//                imm.showSoftInput(mWebView, 0);
+//            }
+//        });
+
+        if (savedInstanceState==null) {
             mWebView.loadUrl("http://192.168.0.106:3000");
         }
     }
 
-    @Override
+        @Override
     protected void onSaveInstanceState(Bundle outState) {
         super.onSaveInstanceState(outState);
         mWebView.saveState(outState);
@@ -104,7 +122,27 @@ public class MainActivity extends Activity {
         @Override
         public boolean shouldOverrideUrlLoading(WebView view, String url) {
             view.loadUrl(url);
+
+            if (url.contains("posts")) {
+
+            }
+
             return false;
+        }
+
+        @Override
+        public void onPageFinished(WebView view, String url) {
+            super.onPageFinished(view, url);
+            // Display the keyboard automatically when relevant
+            InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+            if (url.contains("newPost")) {
+                imm.showSoftInput(view, 0);
+            } else {
+                imm.hideSoftInputFromWindow(view.getWindowToken(), 0);
+            }
+            if (url.contains("post")){
+                imm.hideSoftInputFromWindow(view.getWindowToken(), 0);
+            }
         }
     }
 
